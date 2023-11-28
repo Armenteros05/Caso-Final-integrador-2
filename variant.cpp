96dd339c409261a1e51433217e462fbe03ddd010
Armenteros05 <sarmerod@myuax.com>
2023-11-28 12:03:17 +0100

    modificacion 1

--- variant.cpp
+++ variant.cpp
@@ -3,3 +3,39 @@
 #include <map>
 #include "json11.hpp" // Asegúrate de tener esta biblioteca en tu sistema
 #include "jsonlib.hpp" // No estoy seguro de qué biblioteca es esta, asegúrate de tenerla en tu sistema
+#include <iostream>
+namespace jsonlib {
+    class Json;
+}
+
+enum variant_type { Symbol, Number, List, Proc, Lambda, Cadena };
+
+struct Entorno;
+
+class Variant {
+public:
+    typedef Variant(*proc_type) ( const std::vector<Variant>& );
+    typedef std::vector<Variant>::const_iterator iter;
+    typedef std::map<std::string, Variant> map;
+
+    variant_type type;
+    std::string val;
+    std::vector<Variant> list;
+    proc_type proc;
+    Entorno* env;
+
+    Variant(variant_type type = Symbol) : type(type) , env(0), proc(0) { }
+    Variant(variant_type type, const std::string& val) : type(type), val(val) , env(0) , proc(0) { }
+    Variant(proc_type proc) : type(Proc), proc(proc) , env(0) { }
+
+    std::string to_string();
+    std::string to_json_string();
+    static Variant from_json_string(std::string json);
+    static Variant parse_json(jsonlib::Json job);  // Asegúrate de que esta es la definición correcta de la función
+};
+int main(){
+    Variant v = Variant::from_json_string("{\"type\":\"Symbol\",\"val\":\"+\"}");
+    std::cout << v.to_string() << std::endl;
+    return 0;
+}
+
