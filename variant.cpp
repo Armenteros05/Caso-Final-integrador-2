f09d6f8cb2eb7a1677fe3f1ac4ea028552129014
Armenteros05 <sarmerod@myuax.com>
2023-11-28 12:07:21 +0100

    variant completado

--- variant.cpp
+++ variant.cpp
@@ -2,8 +2,8 @@
 #include <vector>
 #include <string>
 #include <map>
-#include "json11.hpp" // Asegúrate de tener esta biblioteca en tu sistema
-#include "jsonlib.hpp" // No estoy seguro de qué biblioteca es esta, asegúrate de tenerla en tu sistema
+#include "json11.hpp"
+#include "jsonlib.hpp"
 #include <iostream>
 namespace jsonlib {
     class Json;
@@ -25,9 +25,9 @@ public:
     proc_type proc;
     Entorno* env;
 
-    explicit Variant(variant_type type = Symbol) : type(type) , env(0), proc(0) { }
-    Variant(variant_type type, std::string  val) : type(type), val(std::move(val)) , env(0) , proc(0) { }
-    explicit Variant(proc_type proc) : type(Proc), proc(proc) , env(0) { }
+    explicit Variant(variant_type type = Symbol) : type(type) , env(nullptr), proc(nullptr) { }
+    Variant(variant_type type, std::string  val) : type(type), val(std::move(val)) , env(nullptr) , proc(nullptr) { }
+    explicit Variant(proc_type proc) : type(Proc), proc(proc) , env(nullptr) { }
 
 };
 int main(){
