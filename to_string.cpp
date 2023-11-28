898e322cefe895e67a9103579a0ce02928f4eecb
Armenteros05 <sarmerod@myuax.com>
2023-11-28 12:15:29 +0100

    añadido de mi implementación en to_string

--- to_string.cpp
+++ to_string.cpp
@@ -29,7 +29,41 @@ public:
     Variant(variant_type type, std::string  val) : type(type), val(std::move(val)) , env(nullptr) , proc(nullptr) { }
     explicit Variant(proc_type proc) : type(Proc), proc(proc) , env(nullptr) { }
 
+    static std::string to_string();
 };
+std::string Variant::to_string() {
+    std::string Variant::to_string(){
+        switch (type) {
+            case variant_type::Symbol:
+                return val;
+            case variant_type::Number:
+                return std::to_string(std::stoi(val));  // Convierte la cadena a entero antes de convertirla a cadena nuevamente
+            case variant_type::List: {
+                std::string result = "[";
+                for (const auto& element : list) {
+                    result += element.to_string() + ", ";
+                }
+                if (!list.empty()) {
+                    result.pop_back(); // Elimina la última coma
+                    result.pop_back(); // Elimina el espacio después de la coma
+                }
+                result += "]";
+                return result;
+            }
+            case variant_type::Proc:
+                return "Procedure";
+            case variant_type::Lambda:
+                return "Lambda";
+            case variant_type::Cadena:
+                return "Cadena";
+            default:
+                return "UnknownType";
+        }
+    }
+
+    return {};
+}
+
 int main(){
 
 }
