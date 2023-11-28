7c8a3f8ec32c3b6abfa1552901203b1262f58b71
Armenteros05 <sarmerod@myuax.com>
2023-11-28 11:52:00 +0100

    parametros

---
+++ variant.cpp
@@ -0,0 +1,5 @@
+#include <vector>
+#include <string>
+#include <map>
+#include "json11.hpp" // Asegúrate de tener esta biblioteca en tu sistema
+#include "jsonlib.hpp" // No estoy seguro de qué biblioteca es esta, asegúrate de tenerla en tu sistema
