0cd4744d0ddb298dea2c5dea8025da72afc7061d
Armenteros05 <sarmerod@myuax.com>
2023-11-28 12:05:06 +0100

    eliminamos la parted del to string

--- variant.cpp
+++ variant.cpp
@@ -1,3 +1,4 @@
+#include <utility>
 #include <vector>
 #include <string>
 #include <map>
@@ -24,18 +25,11 @@ public:
     proc_type proc;
     Entorno* env;
 
-    Variant(variant_type type = Symbol) : type(type) , env(0), proc(0) { }
-    Variant(variant_type type, const std::string& val) : type(type), val(val) , env(0) , proc(0) { }
-    Variant(proc_type proc) : type(Proc), proc(proc) , env(0) { }
+    explicit Variant(variant_type type = Symbol) : type(type) , env(0), proc(0) { }
+    Variant(variant_type type, std::string  val) : type(type), val(std::move(val)) , env(0) , proc(0) { }
+    explicit Variant(proc_type proc) : type(Proc), proc(proc) , env(0) { }
 
-    std::string to_string();
-    std::string to_json_string();
-    static Variant from_json_string(std::string json);
-    static Variant parse_json(jsonlib::Json job);  // Asegúrate de que esta es la definición correcta de la función
 };
 int main(){
-    Variant v = Variant::from_json_string("{\"type\":\"Symbol\",\"val\":\"+\"}");
-    std::cout << v.to_string() << std::endl;
-    return 0;
-}
 
+}
